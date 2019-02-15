7b9d96f04030ca3711609b45ebd76a35768f614c
Maz B <Mazharrr@users.noreply.github.com>
2019-02-15 10:16:06 -0500

    Minor Dataset overview changes (#493)
    * overview changes for datasets
    
    * if 6 distribution types the 6th will appear
    
    * left sidebar selected css added
    
    * Update ui/src/js/components/dataset/overview/Summary.js
    
    Co-Authored-By: Mazharrr <Mazharrr@users.noreply.github.com>
    
    * requested changes

--- ui/src/js/components/dataset/overview/Summary.js
+++ ui/src/js/components/dataset/overview/Summary.js
@@ -9,12 +9,39 @@ export default class Summary extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            tooltipShown: false,
         };
+        this._resetTooltip = this._resetTooltip.bind(this);
+    }
+    /**
+         *  @param {}
+         *  add event listeners
+     */
+    componentDidMount() {
+        window.addEventListener('click', this._resetTooltip);
+    }
+    /**
+         *  @param {}
+         *  cleanup event listeners
+     */
+    componentWillUnmount() {
+        window.removeEventListener('click', this._resetTooltip);
+    }
+      /**
+     *  @param {event} evt
+     *  resets expanded index state
+     *
+     */
+    _resetTooltip(evt) {
+        if (evt.target.className.indexOf('Summary__info') === -1) {
+            this.setState({ tooltipShown: false });
+        }
     }
     render() {
+        const mangagedType = this.props.isManaged ? 'Managed' : 'Unmanaged';
+        const tooltipText = this.props.isManaged ? 'The contents of a Managed Dataset can be modified via file browser on the data tab.  ' : 'The contents of an Unmanaged Dataset are verified by the Gigantum Client, but managed externally.  ';
         return (
-            <div className="Summary">
+            <div className="Summary Card">
                 <div className="Summary__file-count">
                     <div className="Summary__subheader">Number of Files</div>
                     <div className="Summary__content">{this.props.numFiles}</div>
@@ -34,10 +61,16 @@ export default class Summary extends Component {
                     <ul className="Summary__list">
                         {
                             this.props.fileTypeDistribution.length ?
-                            this.props.fileTypeDistribution.map((type) => {
+                            this.props.fileTypeDistribution.slice(0, 6).map((type, index) => {
+
                                 const splitType = type.split('|');
+                                const adjustedType = splitType[1].length > 10 ? `${splitType[1].slice(0, 7)}...` : splitType[1];
+                                const percentage = Math.round(Number(splitType[0]) * 100);
+                                if ((index === 5) && (this.props.fileTypeDistribution.length !== 6)) {
+                                    return <li key={type}>{`+ ${this.props.fileTypeDistribution.length - 6} other types`}</li>;
+                                }
                                 return (
-                                    <li key={type}>{`${splitType[1]} (${Number(splitType[0]) * 100}%)`}</li>
+                                    <li key={type}>{`${adjustedType} (${percentage}%)`}</li>
                                 );
                             })
                             :
@@ -46,7 +79,27 @@ export default class Summary extends Component {
                     </ul>
                 </div>
                 <div className="Summary__manage-type">
-                    <div className="Summary__header">{this.props.isManaged ? 'Managed' : 'Unmanaged'}</div>
+                    <div className="Summary__header">
+                        {mangagedType}
+                        <div
+                            className="Summary__info"
+                            onClick={() => this.setState({ tooltipShown: !this.state.tooltipShown })}
+                        >
+                            {
+                            this.state.tooltipShown &&
+                            <div className="InfoTooltip summary">
+                                {tooltipText}
+                                <a
+                                    target="_blank"
+                                    href="https://docs.gigantum.com/"
+                                    rel="noopener noreferrer"
+                                >
+                                Learn more.
+                                </a>
+                            </div>
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         );
