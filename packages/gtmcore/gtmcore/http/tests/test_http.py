3d4e2058f50ac2f22b14f01066a8cef8fbee9e27
Dean Kleissas <dean@gigantum.com>
2019-04-29 23:01:27 -0400

    finish tests

--- packages/gtmcore/gtmcore/http/tests/test_http.py
+++ packages/gtmcore/gtmcore/http/tests/test_http.py
@@ -1,12 +1,14 @@
 from gtmcore.http import AsyncHTTPRequestManager, AsyncGetJSON
 import time
+import pytest
+import asyncio
 
 
 class TestAsyncHTTP(object):
-    def test_get_200(self):
+    def test_resolve_many_200(self):
         request1 = AsyncGetJSON('https://jsonplaceholder.typicode.com/todos/1')
         arm = AsyncHTTPRequestManager()
-        result = arm.resolve([request1])
+        result = arm.resolve_many([request1])
         assert isinstance(result, list)
         assert isinstance(result[0], AsyncGetJSON)
         assert 'userId' in result[0].result_json
@@ -23,7 +25,7 @@ class TestAsyncHTTP(object):
 
         t_start = time.time()
         arm = AsyncHTTPRequestManager()
-        result = arm.resolve(request_list)
+        result = arm.resolve_many(request_list)
         t_stop = time.time()
         assert len(result) == 504
 
@@ -33,3 +35,32 @@ class TestAsyncHTTP(object):
         for r in result:
             assert r.status_code == 200
             assert r.result_json == '200 OK'
+
+    def test_resolve_200(self):
+        request1 = AsyncGetJSON('https://jsonplaceholder.typicode.com/todos/1')
+        arm = AsyncHTTPRequestManager()
+        result = arm.resolve(request1)
+        assert isinstance(result, AsyncGetJSON)
+        assert 'userId' in result.result_json
+
+    def test_error(self):
+        req = AsyncGetJSON("https://httpstat.us/404?sleep=100")
+        arm = AsyncHTTPRequestManager()
+        result = arm.resolve(req)
+        assert result.status_code == 404
+        assert result.result_json == '404 Not Found'
+
+    def test_resolve_extraction(self):
+        def test_extractor(data):
+            return data['userId']
+
+        request1 = AsyncGetJSON('https://jsonplaceholder.typicode.com/todos/2', extraction_function=test_extractor)
+        arm = AsyncHTTPRequestManager()
+        result = arm.resolve(request1)
+        assert result.extracted_value == 1
+
+    def test_timeout(self):
+        req = AsyncGetJSON("https://httpstat.us/200?sleep=10000", timeout=1)
+        arm = AsyncHTTPRequestManager()
+        with pytest.raises(asyncio.TimeoutError):
+            arm.resolve(req)
