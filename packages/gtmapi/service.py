f484d31415f7918fab0e96cfc21c8cf412803fe0
jjwatts <jaredwatts@gmail.com>
2019-07-15 17:04:52 +0100

    post_save_hook - issue with images which use Python 3.5 (#903)
    If the base install of Python within a base image happens to be Python 3.5 (due to other constraints in the base image), Jupyter will fail to launch as Python 3.6 specific syntax (string formatting) is included within the post_save_hook that is inserted via the /mnt/share mapped volume.
    
    Signed-off-by: Jared Watts <j.watts@digital-science.com>

--- packages/gtmapi/service.py
+++ packages/gtmapi/service.py
@@ -118,9 +118,9 @@ def post_save_hook(os_path, model, contents_manager, **kwargs):
         labmanager_ip = open('/home/giguser/labmanager_ip').read().strip()
         tokens = open('/home/giguser/jupyter_token').read().strip()
         username, owner, lbname, jupyter_token = tokens.split(',')
-        url_args = f'file={os.path.basename(os_path)}&jupyter_token={jupyter_token}'
-        subprocess.run(['wget', '--spider', 
-            f'http://{labmanager_ip}:10001/api/savehook/{username}/{owner}/{lbname}?{url_args}'], cwd='/tmp')
+        url_args = "file={}&jupyter_token={}".format(os.path.basename(os_path), jupyter_token)
+        url = "http://{}:10001/api/savehook/{}/{}/{}?{}".format(labmanager_ip,username,owner,lbname,url_args)
+        subprocess.run(['wget', '--spider', url], cwd='/tmp')
     except Exception as e:
         print(e)
 
