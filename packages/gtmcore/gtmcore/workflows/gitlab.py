4e0533ef185f9d3819953b6c06875848b01b85ae
fiona <fm.west673@gmail.com>
2020-02-14 14:40:38 -0500

    updated public_jobs to public_builds in create labbook function (#1255)
    Signed-off-by: Fiona West <fiona@gigantum.com>

--- packages/gtmcore/gtmcore/workflows/gitlab.py
+++ packages/gtmcore/gtmcore/workflows/gitlab.py
@@ -289,7 +289,7 @@ class GitLabManager(object):
                 # We want all deconfliction done on client-side.
                 "merge_method": "ff",
                 "visibility": visibility,
-                "public_jobs": False,
+                "public_builds": False,
                 "request_access_enabled": False
                 }
 
