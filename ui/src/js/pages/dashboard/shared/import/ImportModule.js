3495500eca5d5327cb97ababb023cf0ab0bca474
Dean Kleissas <dean@gigantum.com>
2020-11-02 17:54:02 -0500

    use valid characters...

--- ui/src/js/pages/dashboard/shared/import/ImportModule.js
+++ ui/src/js/pages/dashboard/shared/import/ImportModule.js
@@ -14,7 +14,7 @@ import BuildImageMutation from 'Mutations/container/BuildImageMutation';
 import Tooltip from 'Components/tooltip/Tooltip';
 import Modal from 'Components/modal/Modal';
 import LoginPrompt from 'Pages/repository/shared/modals/LoginPrompt';
-import ImportModal from `./modal/ImportModal`;
+import ImportModal from './modal/ImportModal';
 // utilities
 import prepareUpload from './PrepareUpload';
 // assets
